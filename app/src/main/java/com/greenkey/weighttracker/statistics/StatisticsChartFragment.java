05514ffbee6377bc65bb3bd0bdb1c1a4d67384a1
FasterThanSlow <789562115q>
2017-01-15 18:01:05 +0300

    registration 50 percent done

--- app/src/main/java/com/greenkey/weighttracker/statistics/StatisticsChartFragment.java
+++ app/src/main/java/com/greenkey/weighttracker/statistics/StatisticsChartFragment.java
@@ -8,17 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
-import com.github.mikephil.charting.components.YAxis;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.LineData;
-import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
 import com.greenkey.weighttracker.R;
 import com.greenkey.weighttracker.entity.WeightRecord;
+
+import org.eazegraph.lib.charts.BarChart;
+import org.eazegraph.lib.charts.ValueLineChart;
+import org.eazegraph.lib.models.BarModel;
+import org.eazegraph.lib.models.ValueLinePoint;
+import org.eazegraph.lib.models.ValueLineSeries;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -32,13 +29,13 @@ import java.util.concurrent.TimeUnit;
 import io.realm.Realm;
 import io.realm.RealmModel;
 import io.realm.RealmResults;
+import io.realm.Sort;
 
 /**
  * Created by Alexander on 28.12.2016.
  */
 public class StatisticsChartFragment extends Fragment {
 
-    private LineChart chart;
 
     @Nullable
     @Override
@@ -46,54 +43,24 @@ public class StatisticsChartFragment extends Fragment {
         final View view = inflater.inflate(R.layout.statistics_chart_fragment, container, false);
 
         Realm realm = Realm.getDefaultInstance();
-        RealmResults realmResults = realm.where(WeightRecord.class).findAll();
+        RealmResults realmResults = realm.where(WeightRecord.class).findAll().sort("date", Sort.ASCENDING);
 
         if (realmResults.isEmpty()) {
             return view;
         }
 
-        chart = (LineChart) view.findViewById(R.id.chart);
-        List<Entry> entries = new ArrayList<>();
-
         Iterator iterator = realmResults.iterator();
+        ValueLineSeries series = new ValueLineSeries();
+        series.setColor(0xFF56B7F1);
 
         while (iterator.hasNext()) {
             WeightRecord weightRecord = (WeightRecord) iterator.next();
-            entries.add(new Entry(weightRecord.getDate(), weightRecord.getValue()));
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd MMM",Locale.getDefault());
+            series.addPoint(new ValueLinePoint(simpleDateFormat.format(weightRecord.getDate()), weightRecord.getValue()));
         }
-
-        LineDataSet dataSet = new LineDataSet(entries, "Label");
-
-
-        LineData lineData = new LineData(dataSet);
-        chart.setData(lineData);
-        chart.invalidate();
-
-        XAxis xAxis = chart.getXAxis();
-        xAxis.setPosition(XAxis.XAxisPosition.TOP_INSIDE);
-        xAxis.setTextSize(12f);
-        xAxis.setTextColor(Color.WHITE);
-        xAxis.setDrawAxisLine(false);
-        xAxis.setDrawGridLines(true);
-        xAxis.setTextColor(Color.rgb(255, 192, 56));
-        xAxis.setCenterAxisLabels(true);
-        xAxis.setGranularity(24f); // one hour
-        xAxis.setValueFormatter(new IAxisValueFormatter() {
-
-            private SimpleDateFormat mFormat = new SimpleDateFormat("dd MMM HH:mm", Locale.getDefault());
-
-            @Override
-            public String getFormattedValue(float value, AxisBase axis) {
-
-                long millis = (long)value;
-                return mFormat.format(new Date(millis));
-            }
-
-            @Override
-            public int getDecimalDigits() {
-                return 0;
-            }
-        });
+        ValueLineChart mCubicValueLineChart = (ValueLineChart) view.findViewById(R.id.chart);
+        mCubicValueLineChart.addSeries(series);
+        mCubicValueLineChart.startAnimation();
         return view;
     }
 }
